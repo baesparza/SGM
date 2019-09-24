30ee74b0c5fde67d551ae6229bf5e80f8c803d2e
Bruno Esparza <baesparza@iMac-de-utpl-2.local>
2019-09-24 10:04:37 -0500

    feat(modules): add admin, forms and shared module

--- projects/webapp/src/app/app.module.ts
+++ projects/webapp/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AngularFireModule } from '@angular/fire';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    AngularFireModule.initializeApp(environment.firebase, 'webapp')
+    AngularFireModule.initializeApp(environment.firebase, 'webapp'),
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ projects/webapp/src/app/shared/shared.module.ts
@@ -0,0 +1,15 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+
+const FIREBASE_MODULES = [];
+@NgModule({
+  imports: FIREBASE_MODULES,
+  exports: FIREBASE_MODULES
+})
+class FirebaseModule {}
+
+@NgModule({
+  declarations: [],
+  imports: [FirebaseModule],
+  exports: [FirebaseModule]
+})
+export class SharedModule {}
