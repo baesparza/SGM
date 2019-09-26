d63d074d4058b5a0e8f24c92197e41943ebd4c85
Bruno Esparza <bruno.be81@gmail.com>
2019-09-26 00:04:41 -0500

    feat(seguimiento): add auto email and sendgrid

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,41 @@
 const functions = require('firebase-functions');
+const sgMail = require('@sendgrid/mail');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+sgMail.setApiKey(
+  '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'
+);
+
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
+
+exports.helloWorld = functions.https.onRequest((request, response) => {
+  response.send('Hello from Firebase!');
+});
+
+//@ts-check
+exports.mailSeguimientoForm = functions.firestore
+  .document('forms/seguimiento/responses/{id}')
+  .onWrite(async (change, context) => {
+    const responseData = change.after.data();
+    const responseId = context.params.id;
+
+    const msg = {
+      to: `${responseData.studentUsername}@utpl.edu.ec`,
+      from: 'bruno.be81@gmail.com',
+      subject: 'Validación seguimiento - Proyecto Mentores',
+      html: `
+      <p>
+      Hola ${responseData.studentName}
+      </p>
+      <p>
+      Este es un correo para validar una forma de Seguimiento <b>${responseId}</b> subida por: ${responseData.mentorName}
+      </p>
+      `
+    };
+    try {
+      let res = await sgMail.send(msg);
+      console.log(res);
+    } catch (err) {
+      console.err(err);
+    }
+  });
